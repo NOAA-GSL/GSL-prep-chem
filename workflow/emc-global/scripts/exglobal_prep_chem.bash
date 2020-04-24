bfea2522aa4c2c572b22cedae0aba57ae7034599
Walter Kolczynski <Walter.Kolczynski@noaa.gov>
2020-04-24 20:36:50 +0000

    Fix problem with overriding gbbepx_pattern
    Previous change to split GBBEPX_DATA_DIR into a TODAY and YESTERDAY
    introduced a problem where gbbepx_pattern would have to be changed
    depending on cycle and possibly not even work properly if you
    wanted to override the default. Now, instead of changing the
    gbbepx_pattern depending on the cycle, GBBEPX_DATA_DIR is set to
    either the today or yesterday version and gbbepx_pattern can remain
    unchanged.

--- workflow/emc-global/scripts/exglobal_prep_chem.bash
+++ workflow/emc-global/scripts/exglobal_prep_chem.bash
@@ -134,12 +134,13 @@ cd ..
 
 # Any variables have to be exported to the environment before substitution
 if [[ $SHOUR == 00 ]] ; then
-    gbbepx_pattern=${gbbepx_pattern:-'$GBBEPX_DATA_DIR_YESTERDAY/${local_name}.003.${day}.FV3.${CASE}Grid.${tiledir}.bin'}
+    GBBEPX_DATA_DIR=$GBBEPX_DATA_DIR_YESTERDAY
     export gbbepx_days=${gbbepx_days:-'$PDYm2 $PDYm3'}
 else
-    gbbepx_pattern=${gbbepx_pattern:-'$GBBEPX_DATA_DIR_TODAY/${local_name}.003.${day}.FV3.${CASE}Grid.${tiledir}.bin'}
+    GBBEPX_DATA_DIR=$GBBEPX_DATA_DIR_TODAY    
     export gbbepx_days=${gbbepx_days:-'$PDYm1 $PDYm2'}
 fi
+gbbepx_pattern=${gbbepx_pattern:-'$GBBEPX_DATA_DIR/${local_name}.003.${day}.FV3.${CASE}Grid.${tiledir}.bin'}
 gbbepx_days=$(env envsubst <<< $gbbepx_days)
 for day in $gbbepx_days; do
     set -x  # This region is too verbose for "set -x"
